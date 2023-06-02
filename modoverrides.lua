9a78b82c36e661d25cbdc31e78e0c324ac0b4b5c
dezfl <135324329+dezfl@users.noreply.github.com>
2023-06-02 22:16:22 +0500

    Add files via upload

---
+++ modoverrides.lua
@@ -0,0 +1,16 @@
+return {
+    ["workshop-488018406"] = { enabled = true },
+    ["workshop-1337321458"] = { enabled = true },
+    ["workshop-545315736"] = { enabled = true },
+    ["workshop-2078243581"] = { enabled = true },
+    ["workshop-1185229307"] = { enabled = true },
+    ["workshop-358749986"] = { enabled = true },
+    ["workshop-2075943614"] = { enabled = true },
+    ["workshop-796637258"] = { enabled = true },
+    ["workshop-378160973"] = { enabled = true },
+    ["workshop-462469447"] = { enabled = true },
+    ["workshop-614883197"] = { enabled = true },
+    ["workshop-2713019887"] = { enabled = true },
+    ["workshop-2069206134"] = { enabled = true },
+    ["workshop-362175979"] = { enabled = true }
+}
